662662b6e528a16446f2a559cedeec0e42a53a11
(no author) <(no author)@04f08128-0144-4f3d-8c48-8f3e951837fe>
2010-09-11 12:45:30 +0000

    * fixed table name
    git-svn-id: svn://localhost/jb_eventstaff@25 04f08128-0144-4f3d-8c48-8f3e951837fe

--- dca/tl_calendar_events.php
+++ dca/tl_calendar_events.php
@@ -46,7 +46,7 @@ $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['jbeventstaff_id'] = array (
     'label'         => &$GLOBALS['TL_LANG']['tl_calendar_events']['jbeventstaff_id'],
 	'exclude'       => true,
 	'inputType'     => 'select',
-	'foreignKey'    => 'tl_calendar_staff.title',
+	'foreignKey'    => 'tl_jbeventstaff.title',
 	'eval'          => array('mandatory'=>true)
 );
 
