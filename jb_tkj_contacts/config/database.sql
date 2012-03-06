0ac533d7292f2486ef087f5e3a4a7d82eea060d0
Jens Bertram <code@jens-bertram.net>
2012-03-06 13:26:54 +0100

    import jb_tkj_contacts

---
+++ jb_tkj_contacts/config/database.sql
@@ -0,0 +1,18 @@
+-- **********************************************************
+-- *                                                        *
+-- * IMPORTANT NOTE                                         *
+-- *                                                        *
+-- * Do not import this file manually but use the TYPOlight *
+-- * install tool to create and maintain database tables!   *
+-- *                                                        *
+-- **********************************************************
+
+-- 
+-- Table `tl_module`
+-- 
+CREATE TABLE `tl_module` (
+  `jb_tkjcontacts_data` varchar(255) NOT NULL default '',
+  `jb_tkjcontacts_template` varchar(255) NOT NULL default '',
+  `jb_tkjcontacts_groups` varchar(255) NOT NULL default '',
+  `jb_tkjcontacts_link` char(1) NOT NULL default '',
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ jb_tkj_contacts/languages/de/tl_module.php
@@ -0,0 +1,46 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight webCMS
+ * Copyright (C) 2005 Leo Feyer
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at http://www.gnu.org/licenses/.
+ *
+ * PHP version 5
+ * @copyright  Jens Bertram 2010 
+ * @author     Jens Bertram <code@jens-bertram.net>
+ * @package    JB_TKJ_Contacts 
+ * @license    LGPL 
+ * @filesource
+ */
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_module']['jb_tkjcontacts_groups_legend'] = 'Benutzergruppen';
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['jb_tkjcontacts_groups'] = array(
+	'Benutzergruppen',
+	'Wählen Sie die Benutzergruppen, aus denen Personen angezeigt werden sollen.');
+$GLOBALS['TL_LANG']['tl_module']['jb_tkjcontacts_data'] = array(
+	'Angezeigte Benutzerdaten',
+	'Wählen Sie welche Daten über die Benutzer ausgegeben werden sollen.');
+$GLOBALS['TL_LANG']['tl_module']['jb_tkjcontacts_link'] = array(
+	'Kontakt Link anzeigen',
+	'Legt fest, ob ein Link zur Kontaktseite ausgegeben wird.');
+?>
